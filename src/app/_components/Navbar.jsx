b8af5f065ae52985e9da0d40632e957d46edc4b8
joaootaviofm <joaootaviofm@gmail.com>
2025-02-24 15:54:06 -0300

    changes

---
+++ src/app/_components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+export default function Navbar(){
+
+    const navList = [
+        {item: "Home", path: "#home"}
+    ]
+
+    return (
+        <nav className="bg-[#2B2A27] text-white">
+            
+        </nav>
+    )
+}
